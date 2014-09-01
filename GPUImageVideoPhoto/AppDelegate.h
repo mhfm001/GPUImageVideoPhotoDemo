7fb7de93483bbf8896c6ceb6353a41787cbecc8e
mhfm001 <maruo@mro-idc.sytes.net>
2014-09-01 23:06:25 +0900

    Copyright Cariya Japan Inc.

--- GPUImageVideoPhoto/AppDelegate.h
+++ GPUImageVideoPhoto/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  GPUImageVideoPhoto
 //
-//  Created by Administrator on 2014/09/01.
-//  Copyright (c) 2014年 mhfm001. All rights reserved.
+//  Created by Hirofumi Maruo on 2014/09/01.
+//  Copyright (c) 2014年 Cariya Japan Inc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
